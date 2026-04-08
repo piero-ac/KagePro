bf0ed76f48d98011efc0645b1b90c91cdd9a8c6c
Piero Coronado Flores <pacoronado01@gmail.com>
2026-04-08 10:50:53 -0400

    Add sessions router

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routes import immersion
+from routes import sessions
 
 
 app = FastAPI()
@@ -12,7 +12,7 @@ app.add_middleware(CORSMiddleware,
                    allow_headers=["*"]
                 )
 
-app.include_router(immersion.router)
+app.include_router(sessions.router)
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
