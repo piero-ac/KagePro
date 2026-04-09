02c9507d1586768738fb340a6927c623e5452e1e
Piero Coronado Flores <pacoronado01@gmail.com>
2026-04-09 17:51:17 -0400

    Rename models

--- backend/database/models.py
+++ backend/database/models.py
@@ -1,9 +1,9 @@
-from db import Base
+from .db import Base
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.dialects.postgresql import UUID
 import uuid
 
-class Session(Base):
+class SessionModel(Base):
     __tablename__ = "session"
     session_id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
     user_id = Column(String, index=True)
@@ -12,7 +12,7 @@ class Session(Base):
     duration = Column(Integer)
     activity_type = Column(String)
 
-class SessionDetails(Base):
+class SessionDetailsModel(Base):
     __tablename__ = "session_details"
     session_details_id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
     audio_id = Column(String)
