7900ab6feb2149471170eb910aece7b168467f5f
Piero Coronado Flores <pacoronado01@gmail.com>
2026-04-09 17:51:00 -0400

    Create health endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy import text
+from typing import Annotated
+from sqlalchemy.orm import Session
 from routes import sessions
+from database.db import get_db
 
+db_dependency = Annotated[Session, Depends(get_db)]
 
 app = FastAPI()
 
@@ -16,3 +21,8 @@ app.include_router(sessions.router)
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/health")
+def health_check(db: db_dependency):
+    db.execute(text("SELECT 1"))
+    return {"status": "ok"}
