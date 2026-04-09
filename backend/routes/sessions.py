36ccb0119045f0e5572d4c1285d0b4bf601f4e8e
Piero Coronado Flores <pacoronado01@gmail.com>
2026-04-09 17:51:54 -0400

    Refactor GET endpoints for session and session details

--- backend/routes/sessions.py
+++ backend/routes/sessions.py
@@ -1,5 +1,10 @@
+from uuid import UUID
 from fastapi import APIRouter, Depends, HTTPException, Request
-from ..utils import auth
+from typing import Annotated
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+from database.models import SessionModel, SessionDetailsModel
+from database.db import get_db
 
 # from ..utils import auth
 #   user_details = auth.authenticate_and_get_user_details(request)
@@ -10,14 +15,29 @@ router = APIRouter(
     tags=["sessions"],
 )
 
+db_dependency = Annotated[Session, Depends(get_db)]
+
 @router.get("/")
 async def get_sessions(request: Request):
     return "Sessions Endpoint"
 
 @router.get("/{session_id}")
-async def get_session(request: Request, session_id: str):
-    return {session_id: session_id}
+async def get_session(db:db_dependency, session_id: UUID):
+    # TODO: check for valid user
+    result = db.execute(
+        select(SessionModel)
+        .where(SessionModel.session_id == session_id)
+        .filter(SessionModel.user_id == "user_test_123") # get user id from clerk auth
+    )
+    session = result.scalars().first()
+    return session
 
 @router.get("/{session_id}/details")
-async def get_session_details(request: Request, session_id: str):
-    return {session_id: session_id}
+async def get_session_details(db:db_dependency, session_id: UUID):
+    # TODO: check for valid user
+    result = db.execute(
+        select(SessionDetailsModel)
+        .where(SessionDetailsModel.session_id == session_id)
+    )
+    session_details = result.scalars().first()
+    return session_details
