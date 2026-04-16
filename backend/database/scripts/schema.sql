4018f6a3ef2872a1a6a5ce6e981b32d7f448c458
Piero Coronado Flores <pacoronado01@gmail.com>
2026-04-16 13:20:34 -0400

    Update schema to have a catalog table

--- backend/database/scripts/schema.sql
+++ backend/database/scripts/schema.sql
@@ -1,3 +1,11 @@
+CREATE TABLE catalog (
+    audio_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    title VARCHAR(100) NOT NULL,
+    language VARCHAR(20) NOT NULL,
+    duration INT,
+    s3_key VARCHAR(255) NOT NULL
+);
+
 CREATE TABLE session (
     session_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     user_id VARCHAR NOT NULL,
@@ -9,9 +17,9 @@ CREATE TABLE session (
 
 CREATE TABLE session_details (
     session_details_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    audio_id VARCHAR(50),
-    user_audio_id VARCHAR(50),
+    session_id UUID REFERENCES session(session_id) ON DELETE CASCADE,
+    audio_id UUID REFERENCES catalog(audio_id) ON DELETE SET NULL,
+    user_audio_id VARCHAR(255),
     title VARCHAR(100),
-    description VARCHAR(300),
-    session_id UUID REFERENCES session(session_id)
+    description VARCHAR(300)
 );
